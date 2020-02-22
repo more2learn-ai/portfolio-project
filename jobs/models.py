c993ceb07d8e21fb8d1961d01b2861e552d3c978
Biju <niranjana2008@gmail.com>
2020-02-22 22:38:49 +0530

    Added bootstrap and jobs are coming up

--- jobs/models.py
+++ jobs/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summmary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=200)
 
